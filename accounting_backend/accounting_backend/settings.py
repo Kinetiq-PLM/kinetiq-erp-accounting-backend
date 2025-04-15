8ee31020ac75afecafed72901ee95e3cb6afd161
l-uci-d <s.abenoja04@gmail.com>
2025-04-16 00:01:35 +0800

    added zappa_settings

--- accounting_backend/accounting_backend/settings.py
+++ accounting_backend/accounting_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)7)=%-6l11jpi_1umxnif^7^_&qsq)quw*+imz6k5qr87qkp__
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
