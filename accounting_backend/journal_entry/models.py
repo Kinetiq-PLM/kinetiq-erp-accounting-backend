a6b505abce160b8ab096f40554d40f15b88824d2
wkwk08 <jerichoambrocio123@gmail.com>
2025-04-15 23:39:42 +0800

    fixing only

--- accounting_backend/journal_entry/models.py
+++ accounting_backend/journal_entry/models.py
@@ -4,8 +4,8 @@ class JournalEntry(models.Model):
     journal_id = models.CharField(primary_key=True, max_length=255)
     journal_date = models.DateField(default='2025-01-01')
     description = models.CharField(max_length=255)
-    total_debit = models.DecimalField(max_digits=15, decimal_places=2)
-    total_credit = models.DecimalField(max_digits=15, decimal_places=2)
+    total_debit = models.DecimalField(max_digits=15, decimal_places=2,  default='0.00')
+    total_credit = models.DecimalField(max_digits=15, decimal_places=2, default='0.00')
     invoice_id = models.CharField(max_length=255, blank=True, null=True)
     currency_id = models.CharField(max_length=255, default='1')
 
