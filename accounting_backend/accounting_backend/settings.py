60010dd509e4f2cb42ca53e14bae16cc4b4b5993
wkwk08 <jerichoambrocio123@gmail.com>
2025-04-15 23:39:27 +0800

    change db to rds

--- accounting_backend/accounting_backend/settings.py
+++ accounting_backend/accounting_backend/settings.py
@@ -100,19 +100,34 @@ WSGI_APPLICATION = 'accounting_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'accounting_module',
+#        'USER': 'postgres',
+#        'PASSWORD': 'zxcxcz.',
+#        'HOST': 'localhost',
+#        'PORT': '5432',
+#        'OPTIONS': {
+#            'options': '-c search_path=accounting'
+#        }
+#    }
+#}
+
+import os
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'accounting_module',
-        'USER': 'postgres',
-        'PASSWORD': 'zxcxcz.',
-        'HOST': 'localhost',
-        'PORT': '5432',
-        'OPTIONS': {
-            'options': '-c search_path=accounting'
-        }
+        'NAME': os.getenv('DB_NAME', 'Kinetiq-DB'),
+        'USER': os.getenv('DB_USER', 'postgres'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'KntBg3jIY0DbpH8G9bwt'),
+        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
+        'PORT': os.getenv('DB_PORT', '15432'),
     }
 }
+
+
 
 
 # Password validation
