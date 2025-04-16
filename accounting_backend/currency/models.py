688bcc742e0961960b194896c533123d11c1c1f5
wkwk08 <jerichoambrocio123@gmail.com>
2025-04-16 19:00:06 +0800

    this model is set to not be managed by django

--- accounting_backend/currency/models.py
+++ accounting_backend/currency/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
 class Currency(models.Model):
-    currency_id = models.CharField(max_length=255, primary_key=True)
-    currency_name = models.CharField(max_length=255)
+    currency_id = models.CharField(max_length=255, primary_key=True, default='currency_id')
+    currency_name = models.CharField(max_length=255, default='currency_name')
     exchange_rate = models.DecimalField(max_digits=15, decimal_places=6)
     is_active = models.BooleanField(default=True)
 
     class Meta:
-        db_table = '"admin"."currency"'  # Explicitly reference admin.currency
+        db_table = '"admin"."currency"'
+        managed = False
