ce65a6d942a29efd89a55b89d2e4270aab58d4da
wkwk08 <jerichoambrocio123@gmail.com>
2025-04-17 14:20:23 +0800

    deleted unique

--- accounting_backend/general_ledger/models.py
+++ accounting_backend/general_ledger/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 class GeneralLedgerAccount(models.Model):
-    gl_account_id = models.CharField(primary_key=True, max_length=255, unique=True)
+    gl_account_id = models.CharField(primary_key=True, max_length=255)
     account_name = models.CharField(max_length=255)
-    account_code = models.CharField(max_length=255, unique=True)  # Mark as unique
+    account_code = models.CharField(max_length=255)
     account_id = models.CharField(max_length=255, null=True, blank=True)
-    status = models.CharField(max_length=50)  # Assuming status_enum is handled as a CharField
-    created_at = models.DateTimeField()
+    status = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         db_table = "general_ledger_accounts"
