75f267143201ac0fb26b13f363d17dcb9c547b54
wkwk08 <jerichoambrocio123@gmail.com>
2025-05-01 16:13:42 +0800

    Change URL paths from snake_case to kebab-case for payroll accounting

--- accounting_backend/payroll_accounting/urls.py
+++ accounting_backend/payroll_accounting/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import PayrollAccountingListCreateView, PayrollAccountingRetrieveUpdateView
 
 urlpatterns = [
-    path('payroll_accounting/', PayrollAccountingListCreateView.as_view(), name='payroll_accounting_list_create'),
-    path('payroll_accounting/<str:pk>/', PayrollAccountingRetrieveUpdateView.as_view(), name='payroll_accounting_detail'),
+    path('payroll-accounting/', PayrollAccountingListCreateView.as_view(), name='payroll_accounting_list_create'),
+    path('payroll-accounting/<str:pk>/', PayrollAccountingRetrieveUpdateView.as_view(), name='payroll_accounting_detail'),
 ]
