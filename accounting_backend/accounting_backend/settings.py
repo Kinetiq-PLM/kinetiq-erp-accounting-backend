dbe751c8ba678d90022b3d8e68489efc5200a9c9
wkwk08 <jerichoambrocio123@gmail.com>
2025-05-05 02:19:52 +0800

    Update settings.py for production: disable DEBUG, set CORS to False, and use SECRET_KEY from env

--- accounting_backend/accounting_backend/settings.py
+++ accounting_backend/accounting_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,11 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)7)=%-6l11jpi_1umxnif^7^_&qsq)quw*+imz6k5qr87qkp__'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'unsafe-fallback-dev-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = False
+DEBUG = False
 
 ALLOWED_HOSTS = ['vyr3yqctq8.execute-api.ap-southeast-1.amazonaws.com',
                 "127.0.0.1",
@@ -74,7 +74,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware'
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = False
 
 
 REST_FRAMEWORK = {
