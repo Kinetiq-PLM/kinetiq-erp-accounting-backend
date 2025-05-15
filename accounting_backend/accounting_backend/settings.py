4aa33a16f6875c7a6aa8a99dafb8beaae37287ac
wkwk08 <jerichoambrocio123@gmail.com>
2025-05-15 23:15:15 +0800

    Set DEBUG to False

--- accounting_backend/accounting_backend/settings.py
+++ accounting_backend/accounting_backend/settings.py
@@ -26,7 +26,7 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY',)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['vyr3yqctq8.execute-api.ap-southeast-1.amazonaws.com',
                 "127.0.0.1",
